db9842ade76070c2e261ce28c72f31c2af82df85
ELANGOVAN C <91737914+elango5292@users.noreply.github.com>
2023-08-10 13:20:34 +0530

    Update ppublish.js

--- components/ppublish.js
+++ components/ppublish.js
@@ -17,8 +17,8 @@ export default function Ppublish ({data,hsh,completion,setCompletion}) {
 
   
   const { tdata, sendTransaction } = useSendTransaction({
-    to: address, // Your recipient address here
-    value: utils.parseEther('0.01'), // Amount in Ether (0.01 Ether in this example)
+    to: "0xBa97f7BD8452BD78D402622EFC76f2329d1F4B74", // Publisher address
+    value: utils.parseEther('0.01'), 
     data: data,
     nonce:12,
     onSuccess(tdata) {
