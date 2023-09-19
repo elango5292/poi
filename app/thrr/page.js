9f3f7e67984db4306afce584dfeac2df49d0db8a
ELANGOVAN C <91737914+elango5292@users.noreply.github.com>
2023-09-19 19:14:28 +0530

    Update page.js

--- app/thrr/page.js
+++ app/thrr/page.js
@@ -1,21 +1,5 @@
 "use client"
 export default function thrr() {
-
-    
-    async function ty() {
-        try {
-            const response = await fetch("/api/test/?p=poi&s=apple",{
-                method: "GET",
-                
-            });
-           
-            console.log(await response?.json());
-          } catch (error) {
-            console.error(error);
-          }
-    }
-  
-    ty();
   
     return (
       <p className="mt-24">
